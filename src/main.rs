3dd6237f0d67df329927712c8673b5be98ef8dff
Ujjwal Sharma <ryzokuken@disroot.org>
2021-08-04 01:50:39 +0530

    read config using serde and serde_json

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use serde::{Deserialize, Serialize};
+
 #[async_std::main]
 async fn main() -> Result<(), std::io::Error> {
+    let config = read_config();
     tide::log::start();
     let mut app = tide::new();
     app.at("/").get(root);
@@ -7,12 +10,34 @@ async fn main() -> Result<(), std::io::Error> {
     Ok(())
 }
 
-fn read_config() -> String {
+#[derive(Serialize, Deserialize)]
+struct IMAPConfig {
+    username: String,
+    password: String,
+    host: String,
+    port: u16,
+    tls: bool
+}
+
+#[derive(Serialize, Deserialize)]
+struct JMAPConfig {
+    username: String,
+    password: String
+}
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    imap: IMAPConfig,
+    jmap: JMAPConfig
+}
+
+fn read_config() -> Config {
     let config_dir = dirs::config_dir().unwrap();
     let config_path = config_dir.join("jmap-proxy/config.json");
-    std::fs::read_to_string(config_path).unwrap()
+    let config_str = std::fs::read_to_string(config_path).unwrap();
+    serde_json::from_str(&config_str).unwrap()
 }
 
 async fn root(_req: tide::Request<()>) -> tide::Result<String> {
-    Ok(read_config())
+    Ok(String::from("hello"))
 }
