405892a55e3d5637928ed433c5a5700a7f376e85
Ujjwal Sharma <ryzokuken@disroot.org>
2021-08-04 00:25:52 +0530

    add dirs and read config file

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,17 @@
 async fn main() -> Result<(), std::io::Error> {
     tide::log::start();
     let mut app = tide::new();
-    app.at("/").get(|_| async { Ok("Hello, world!") });
+    app.at("/").get(root);
     app.listen("127.0.0.1:8080").await?;
     Ok(())
+}
+
+fn read_config() -> String {
+    let config_dir = dirs::config_dir().unwrap();
+    let config_path = config_dir.join("jmap-proxy/config.json");
+    std::fs::read_to_string(config_path).unwrap()
+}
+
+async fn root(_req: tide::Request<()>) -> tide::Result<String> {
+    Ok(read_config())
 }
